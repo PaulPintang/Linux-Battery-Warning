0a164c54064dfe3a3a794af81e3b478c0a91b875
PaulPintang <pauljpintang@gmail.com>
2022-03-26 22:07:34 +0800

    first commit

---
+++ battery-warning.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+BATTERY=$(upower -e | grep 'BAT')
+
+while :
+do
+    BATTERY_PERCENTAGE=$(upower -i $BATTERY | grep percentage | awk '{ print $2 }'| sed s/'%'/''/g)
+    CABLE=$(upower -d | grep -n2 line-power | grep online | awk '{ print $3 }')
+
+    if [[ "$BATTERY_PERCENTAGE" -lt "40" && $CABLE = "no" ]]; then
+        notify-send "WARNING: Your battery is 40% below!" "NOTE: Keep it charged between 40% and 80% for better battery lifespan :)" -u critical;
+        sleep 20
+    elif [[ "$BATTERY_PERCENTAGE" -ge "80" ]]; then
+        notify-send "Battery is above 80%" "NOTE: Keep it charged between 40% and 80% for better battery lifespan :) " -u low
+        sleep 20
+    elif [[ "$BATTERY_PERCENTAGE" -ge "98" ]]; then
+        notify-send "Battery Full: 100%" "Please disconnect your charger!" -u low
+        sleep 20
+    fi
+
+sleep 60
+done
