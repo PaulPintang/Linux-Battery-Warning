9e33c46beeadb7091b5acb2eae6eae1a3261e623
PaulPintang <pauljpintang@gmail.com>
2022-03-27 15:18:47 +0800

    add also a condition if the adapter is connected or not

--- battery-warning.sh
+++ battery-warning.sh
@@ -9,10 +9,10 @@ do
     if [[ "$BATTERY_PERCENTAGE" -lt "40" && $CABLE = "no" ]]; then
         notify-send "WARNING: Your battery is 40% below!" "NOTE: Keep it charged between 40% and 80% for better battery lifespan :)" -u critical;
         sleep 20
-    elif [[ "$BATTERY_PERCENTAGE" -ge "80" ]]; then
+    elif [[ "$BATTERY_PERCENTAGE" -ge "80" && $CABLE = "no" ]]; then
         notify-send "Battery is above 80%" "NOTE: Keep it charged between 40% and 80% for better battery lifespan :) " -u low
         sleep 20
-    elif [[ "$BATTERY_PERCENTAGE" -ge "98" ]]; then
+    elif [[ "$BATTERY_PERCENTAGE" -ge "98" && $CABLE = "no" ]]; then
         notify-send "Battery Full: 100%" "Please disconnect your charger!" -u low
         sleep 20
     fi
