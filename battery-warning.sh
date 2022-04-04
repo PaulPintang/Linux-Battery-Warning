3a01c376ebb056ea9e06aa28ae2d2fe338a9817b
PaulPintang <pauljpintang@gmail.com>
2022-04-04 22:40:46 +0800

    add sound alert: low and full charged

--- battery-warning.sh
+++ battery-warning.sh
@@ -6,16 +6,16 @@ do
     BATTERY_PERCENTAGE=$(upower -i $BATTERY | grep percentage | awk '{ print $2 }'| sed s/'%'/''/g)
     CABLE=$(upower -d | grep -n2 line-power | grep online | awk '{ print $3 }')
 
-    if [[ "$BATTERY_PERCENTAGE" -lt "40" && $CABLE = "no" ]]; then
+    if [[ "$BATTERY_PERCENTAGE" -le "40" && $CABLE = "no" ]]; then
         notify-send "WARNING: Your battery is 40% below!" "NOTE: Keep it charged between 40% and 80% for better battery lifespan :)" -u critical;
-        sleep 20
+        paplay Downloads/low.wav
     elif [[ "$BATTERY_PERCENTAGE" -ge "80" && $CABLE = "yes" ]]; then
         notify-send "Battery is above 80%" "NOTE: Keep it charged between 40% and 80% for better battery lifespan :) " -u low
-        sleep 20
+        paplay Downloads/full.wav
     elif [[ "$BATTERY_PERCENTAGE" -ge "98" && $CABLE = "yes" ]]; then
         notify-send "Battery Full: 100%" "Please disconnect your charger!" -u low
-        sleep 20
+		paplay Downloads/full.wav
     fi
 
-sleep 60
+sleep 30
 done
